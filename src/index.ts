47b7586b61ca2326795e67acee4ba1f48e333e18
Adrian Falleiro <adrian.falleiro@classdojo.com>
2025-05-06 14:23:10 +0000

    Update exports to raw client

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
 export { Vitess } from "./lib/vitess";
 export { createBindVariable } from "./lib/encoder";
-
-export { UpdateStreamClient } from "./gen/binlogservice_client";
-export { MysqlCtlClient } from "./gen/mysqlctl_client";
-export { QueryClient } from "./gen/queryservice_client";
-export { TabletManagerClient } from "./gen/tabletmanagerservice_client";
-export { ThrottlerClient } from "./gen/throttlerservice_client";
-export { VTAdminClient } from "./gen/vtadmin_client";
-export { VtctlClient, VtctldClient } from "./gen/vtctlservice_client";
+export * as RawClient from "./lib/rawClient";
---
+++ src/lib/rawClient.ts
@@ -0,0 +1,8 @@
+export { UpdateStreamClient } from "../gen/binlogservice_client";
+export { MysqlCtlClient } from "../gen/mysqlctl_client";
+export { QueryClient } from "../gen/queryservice_client";
+export { TabletManagerClient } from "../gen/tabletmanagerservice_client";
+export { ThrottlerClient } from "../gen/throttlerservice_client";
+export { VTAdminClient } from "../gen/vtadmin_client";
+export { VtctlClient, VtctldClient } from "../gen/vtctlservice_client";
+export { VitessClient } from "../gen/vtgateservice_client";
